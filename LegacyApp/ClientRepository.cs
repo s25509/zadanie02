c072e844089d45b040d9ed2a07f78b39b196e320
s25509 <s25509@pjwstk.edu.pl>
2024-04-06 12:37:45 +0200

    changed Database accessibility

--- LegacyApp/ClientRepository.cs
+++ LegacyApp/ClientRepository.cs
@@ -9,7 +9,7 @@ namespace LegacyApp
         /// <summary>
         /// This collection is used to simulate remote database
         /// </summary>
-        public static readonly Dictionary<int, Client> Database = new Dictionary<int, Client>()
+        private static readonly Dictionary<int, Client> Database = new Dictionary<int, Client>()
         {
             {1, new Client{ClientId = 1, Name = "Kowalski", Address = "Warszawa, Złota 12", Email = "kowalski@wp.pl", Type = "NormalClient"}},
             {2, new Client{ClientId = 2, Name = "Malewski", Address = "Warszawa, Koszykowa 86", Email = "malewski@gmail.pl", Type = "VeryImportantClient"}},
