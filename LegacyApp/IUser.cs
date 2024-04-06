27f710f33904abda53a6b6369437cb503ef397e9
s25509 <s25509@pjwstk.edu.pl>
2024-04-06 13:21:30 +0200

    added some User abstraction

---
+++ LegacyApp/IUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public interface IUser
+    {
+        object Client { get; }
+        DateTime DateOfBirth { get; }
+        string EmailAddress { get; }
+        string FirstName { get; }
+        string LastName { get; }
+        bool HasCreditLimit { get; }
+        int CreditLimit { get; }
+    }
+}
--- LegacyApp/User.cs
+++ LegacyApp/User.cs
@@ -2,13 +2,13 @@
 
 namespace LegacyApp
 {
-    public class User
+    public class User : IUser
     {
-        public object Client { get; internal set; }
-        public DateTime DateOfBirth { get; internal set; }
+        public object Client { get; init; }
+        public DateTime DateOfBirth { get; init; }
         public string EmailAddress { get; internal set; }
-        public string FirstName { get; internal set; }
-        public string LastName { get; internal set; }
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
         public bool HasCreditLimit { get; internal set; }
         public int CreditLimit { get; internal set; }
     }
